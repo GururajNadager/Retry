e8126dd37213af5ab7fdc1c5b0cc15ef9e61b06f
Gururaj Nadager <Gururaj.Nadager@happiestminds.com>
2018-07-19 17:50:52 +0530

    implemented logging support

--- Retry/Retry.cs
+++ Retry/Retry.cs
@@ -11,6 +11,21 @@ namespace Retry
     public static class Retry
     {
         /// <summary>
+        /// When Enabled,Calls the Appropriate Logger Method
+        /// </summary>
+        public static bool EnableLogging { get; set; }
+
+        /// <summary>
+        /// Delegate Method for Info Logger Method
+        /// </summary>
+        public static Action<string> InfoLogAction { get; set; }
+
+        /// <summary>
+        /// Delegate Method for Exception Logger Method
+        /// </summary>
+        public static Action<Exception> ExceptionLogAction { get; set; }
+
+        /// <summary>
         /// Retry the specified operation the specified number of times, until there are no more retries or it succeeded
         /// without an exception.
         /// </summary>
@@ -44,26 +59,46 @@ namespace Retry
                 {
                     if (retry > 0)
                     {
+                        LogInfo($"Retrying Method Execution {operation.Method.Name} :retry attempt ({retry} of {retry-1})");
+
                         Thread.Sleep(retryDelay);
+
+                        LogInfo($"Retrying Method Execution {operation.Method.Name} in {retryDelay.Hours} hour {retryDelay.Minutes} minutes {retryDelay.Seconds} seconds");
                     }
+
+                    LogInfo($"Started Executing Method {operation.Method.Name} at {DateTime.Now}");
 
                     // Execute method
                     operation();
 
                     //If the Control reaches this line then the Task is Success:
                     taskResult.Status = TaskStatus.Success;
+                    LogInfo($"Method Execution {operation.Method.Name} success");
+
                     break;
                 }
                 catch (Exception ex)
                 {
-                    taskResult.Exceptions.Add(ex);
+                    LogInfo($"Method Execution {operation.Method.Name} failed");
 
-                    if (!retryOnExceptionType.Select(x => x.InnerException.GetType()).Contains(ex.GetType()))
+                    if (!retryOnExceptionType.Select(x => x.GetType().FullName).Contains(ex.GetType().FullName))
                         break;
                 }
             }
 
+            LogInfo($"Method Execution {operation.Method.Name} completed at {DateTime.Now}");
+
             return taskResult;
+        }
+
+        /// <summary>
+        /// Calls the Logger Actions if Logging is enabled
+        /// </summary>
+        /// <param name="message">message to log</param>
+        private static void LogInfo(string message)
+        {
+            if(EnableLogging)
+                InfoLogAction(message);
         }
     }
 }
